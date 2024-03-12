ef7b65fff50eb44187ba5705894b264613a8b21e
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-12 13:06:25 -0500

    Guess the Number functionality

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
 "use strict";
-console.log("JS is working");
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const againButton = document.querySelector(".again");
+document.querySelector(".score").textContent = score;
+
+//Update Message
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+//Check Guess Functionality
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  //When invalid input given
+  if (!guess) {
+    displayMessage("Invalid Guess");
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    displayMessage("Correct!!! You Win!");
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? "Too High! Guess Again" : "Too Low! Guess Again"
+      );
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You Lose!");
+    }
+  }
+});
+
+//New Game Functionality
+againButton.addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector(".score").textContent = score;
+  displayMessage("Start guessing...");
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
